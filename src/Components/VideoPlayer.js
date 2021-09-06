dbbe95b6e1428a818f19f33eb28feff507597946
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2021-09-06 13:26:04 +0530

    fix up

--- src/Components/VideoPlayer.js
+++ src/Components/VideoPlayer.js
@@ -1,23 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactPlayer from 'react-player'
+
 import './VideoPlayer.css'
 
 const VideoPlayer = () => {
+  const [isplay, setIsPlay] = useState(false)
   return (
     <div className='Container'>
       <div className='playerContainer'>
         <ReactPlayer
           url='https://www.youtube.com/watch?v=zln8SZO6yYY'
           controls='true'
+          playing={isplay}
         />
       </div>
-      <div className='circle'>
-        <ReactPlayer
-          url='https://www.youtube.com/watch?v=zln8SZO6yYY'
-          controls='true'
-          height='100%'
-          width='100%'
-        />
+      <div className='circle' onClick={() => setIsPlay(preValue => !preValue)}>
+        {isplay ? (
+          <i class='far fa-pause-circle fa-3x'></i>
+        ) : (
+          <i class='far fa-play-circle fa-3x'></i>
+        )}
       </div>
     </div>
   )
